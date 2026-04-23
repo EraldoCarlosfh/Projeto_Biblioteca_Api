b57fb165c3959c4b36a3edc070a9a76505ef0998
Eraldo Oliveira <eraldo1904@gmail.com>
2026-04-23 17:12:21 -0300

    Criado Projeto Biblioteca Api

---
+++ Biblioteca.Api/Controllers/WeatherForecastController.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Biblioteca.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly Random _random = new Random(); // .NET 5 não tem Random.Shared
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index), // .NET 5 usa DateTime
+                TemperatureC = _random.Next(-20, 55),
+                Summary = Summaries[_random.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        public class WeatherForecast
+        {
+            public DateTime Date { get; set; }
+
+            public int TemperatureC { get; set; }
+
+            public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+            public string Summary { get; set; }
+        }
+    }
+}
---
+++ Biblioteca.Infrastructure/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Biblioteca.Infrastructure
+{
+    public class Class1
+    {
+    }
+}
